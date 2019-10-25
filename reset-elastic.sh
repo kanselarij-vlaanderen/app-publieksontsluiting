cc44f8384fd66d6a6500b324247c807269f8b177
Michaël Dierick <michael.dierick@redpencil.io>
2019-10-25 15:05:45 +0200

    Fix kibana part in elastic reset script

--- reset-elastic.sh
+++ reset-elastic.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "warning this will run queries on the triplestore and delete containers, you have 3 seconds to press ctrl+c"
 sleep 3
-docker-compose rm -fs elasticsearch search kibana
+docker-compose rm -fs elasticsearch search
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml rm -fs kibana
 sudo rm -rf data/elasticsearch/
 docker-compose exec -T virtuoso isql-v <<EOF
 SPARQL DELETE WHERE {   GRAPH <http://mu.semte.ch/authorization> {     ?s ?p ?o.   } };
