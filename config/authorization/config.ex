f4d94d34bcd23ea0d0172926d34c4927a5f057a6
Flor Sanders <me@florsanders.be>
2021-05-07 11:58:37 +0200

    [authorization] Updated list of public resource since last updates to data model

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -155,23 +155,25 @@ defmodule Acl.UserGroups.Config do
           graph: "http://mu.semte.ch/graphs/public",
           constraint: %ResourceConstraint{
             resource_types: [
+              "http://data.vlaanderen.be/ns/besluit#Agendapunt",
+              "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+              "http://data.vlaanderen.be/ns/besluit#Vergaderactiviteit",
+              "http://data.vlaanderen.be/ns/mandaat#Mandaat",
               "http://data.vlaanderen.be/ns/mandaat#Mandataris",
-              "http://www.w3.org/ns/person#Person",
+              "http://data.vlaanderen.be/ns/mandaat#RechtstreekseVerkiezing",
+              "http://mu.semte.ch/vocabularies/ext/Nieuwsbericht",
+              "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
               "http://www.w3.org/2004/02/skos/core#Concept",
               "http://www.w3.org/2004/02/skos/core#ConceptScheme",
-              "http://data.vlaanderen.be/ns/besluit#Agendapunt",
-              "https://data.vlaanderen.be/ns/dossier#Stuk",
-              "https://data.vlaanderen.be/ns/besluitvorming#Agenda",
-              "http://data.vlaanderen.be/ns/besluit#Vergaderactiviteit",
+              "http://www.w3.org/ns/org#Role",
+              "http://www.w3.org/ns/person#Person",
               "http://www.w3.org/ns/prov#Activity",
               "http://www.w3.org/ns/prov#Generation",
-              "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
-              "http://data.vlaanderen.be/ns/mandaat#RechtstreekseVerkiezing",
-              "http://www.w3.org/ns/org#Role",
               "http://www.w3.org/ns/prov#Invalidation",
-              "http://data.vlaanderen.be/ns/mandaat#Mandaat",
-              "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
-              "http://mu.semte.ch/vocabularies/ext/Nieuwsbericht"
+              "https://data.vlaanderen.be/ns/besluitvorming#Agenda",
+              "https://data.vlaanderen.be/ns/dossier#Dossier",
+              "https://data.vlaanderen.be/ns/dossier#Serie",
+              "https://data.vlaanderen.be/ns/dossier#Stuk"
             ]
           } } ]
       },
